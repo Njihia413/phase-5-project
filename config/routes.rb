01f15ab298bba45efa2cd763d4b2428dcf830480
Maureen Njihia <maureennjihia468@gmail.com>
2022-12-19 09:54:13 +0300

    Configure for Render

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # route to test your configuration
+  get '/hello', to: 'application#hello_world'
 end
