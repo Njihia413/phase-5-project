64522969238a2b66f9c009426c165c633052aed0
Maureen Njihia <maureennjihia468@gmail.com>
2023-01-12 10:45:05 +0300

    Make few changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import { Routes, Route }  from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import Footer from "./Footer";
-import ContactUs from "./ContactUs";
+// import ContactUs from "./ContactUs";
 import Courses from "./Courses";
 import About from "./About";
 import CourseDetails from "./CourseDetails";
@@ -19,7 +19,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/contact" element={<ContactUs />} /> 
+          {/* <Route path="/contact" element={<ContactUs />} />  */}
           <Route path="/courses" element={<Courses/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/CourseDetails" element={<CourseDetails/>} />
