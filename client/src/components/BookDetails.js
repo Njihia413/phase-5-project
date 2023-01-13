5820dac65501fa06ccf6d6a29317d8ad215c867d
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2023-01-13 06:26:32 +0300

    ft-header

---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from './Navbar';
+
+const BookDetails = () => {
+  return (
+    <div>
+      <section className="course">
+      <div className="bg-course">
+        <Navbar />
+        <div className="row">
+          <div className="col">
+            <div className="intro">
+              <h1>
+                Get to know more about a course's <br></br> reading materials<br></br>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+</div>
+  )
+}
+export default BookDetails
