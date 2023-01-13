40129289afe4ece59eb11474c39320a090a8877a
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2023-01-13 07:34:06 +0300

    imported review form

--- client/src/pages/Reviews.js
+++ client/src/pages/Reviews.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import ReviewForm from './ReviewForm'
 
 function Reviews() {
   return (
     <div className='review'>
       <h1>Reviews</h1>
+      <ReviewForm/>
     </div>
+
+      
+    
   )
 }
 
