dc158e13a89f57c45003e418e3b3ee31a9523274
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2023-01-16 15:43:13 +0300

    resolving conflicts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,14 +4,14 @@ import { Routes, Route }  from "react-router-dom";
 // import SignUp from "./SignUp";
 import Login from "./Login";
 import Footer from "./Footer";
-import ContactUs from "./ContactUs";
+// import ContactUs from "./ContactUs";
 import Courses from "../pages/Courses";
 // import ContactUs from "./ContactUs";
 // import Courses from "./Courses";
 import About from "./About";
-import Course from "./Course";
-import CourseDetails from "./CourseDetails";
-import Dashboard from "./Dashboard"
+// import Course from "./Course";
+// import CourseDetails from "./CourseDetails";
+// import Dashboard from "./Dashboard"
 
 function App() {
 
@@ -23,12 +23,12 @@ function App() {
           <Route path="/" element={<Home/>}/>
           {/* <Route path="/signup" element={<SignUp />} /> */}
           <Route path="/login" element={<Login />} />
-          <Route path="/contact" element={<ContactUs />} />   
+          {/* <Route path="/contact" element={<ContactUs />} />    */}
           <Route path="/courses" element={<Courses/>} />
           <Route path="/about" element={<About/>} />
-          <Route path="/course" element={<Course/>} />
-          <Route path="/CourseDetails" element={<CourseDetails/>} />
-          <Route path= "/Dashboard" element={<Dashboard/>}/>
+          {/* <Route path="/course" element={<Course/>} />
+          <Route path="/CourseDetails" element={<CourseDetails/>} /> */}
+          {/* <Route path= "/Dashboard" element={<Dashboard/>}/> */}
       </Routes>
       <Footer/>
     </div>
