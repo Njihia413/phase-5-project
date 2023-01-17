e2456bbfda3799df680cc3b3e67ea30e2462133c
ashley <ashley.oketo@student.moringaschool.com>
2023-01-17 20:47:59 +0300

    Added styling for the grade pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,6 +12,7 @@ import Courses from "./Courses";
 //import ContactUs from "./ContactUs";
 import Dashboard from "./Dashboard"
 import CourseDetails from "./CourseDetails";
+import Grades from "../pages/Grades";
 import TeacherDashboard from "../pages/TeacherDashboard";
 
 function App() {
@@ -27,7 +28,8 @@ function App() {
 
             {/* <Route path="/contact" element={<ContactUs />} />     */}
            <Route path="/courses" element={<Courses/>} />  
-          <Route path="/about" element={<About/>} /> 
+          <Route path="/about" element={<About/>} />
+          <Route path="/grades" element={<Grades/>} /> 
            {/* <Route path="/course" element={<Course/>} /> */}
           <Route path="/courses/:courseId" element={<CourseDetails/>} />
           <Route path= "teacherdashboard"  element={<TeacherDashboard/>} />
